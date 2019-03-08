49e4a6ebdbcc2a43d50dabcddf450f5ef5b884bf
bertrand-ravaux <bertrand.ravaux02@gmail.com>
2019-03-08 10:57:17 +0100

    chouette web projet canthon

---
+++ assets/js/projet2.js
@@ -0,0 +1,14 @@
+$(function(){
+      $('#rep1').click(function(){
+        $("#result").html('Essaye encore <img width="150" src="assets/images/sad.png"/>');
+      });
+    });
+    $(function(){
+      $('#rep2').click(function(){
+        $("#result").html('Essaye encore <img width="150" src="assets/images/sad.png"/>');
+      });
+    });$(function(){
+      $('#rep3').click(function(){
+        $("#result").html('Excellente reponse <img width="150" src="assets/images/smile.png"/>');
+      });
+    });
